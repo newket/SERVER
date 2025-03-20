0bc0142525cb644790d56e750093aed3d5123b72
sominyun <somin455@naver.com>
2025-03-21 06:32:30 +0900

    feature:#7 social login, signup 공통으로 처리

--- newket-api/src/main/kotlin/com/newket/api/controller/auth/AuthApi.kt
+++ newket-api/src/main/kotlin/com/newket/api/controller/auth/AuthApi.kt
@@ -4,12 +4,10 @@ object AuthApi {
 
     object V1{
         const val BASE_URL = "/api/v1/auth"
+        const val SOCIAL_LOGIN = "$BASE_URL/login/{provider}"
         const val SOCIAL_LOGIN_KAKAO = "$BASE_URL/login/KAKAO"
-        const val SOCIAL_LOGIN_APPLE = "$BASE_URL/login/APPLE"
-        const val SOCIAL_LOGIN_NAVER = "$BASE_URL/login/NAVER"
+        const val SIGNUP = "$BASE_URL/signup/{provider}"
         const val SIGNUP_KAKAO = "$BASE_URL/signup/KAKAO"
-        const val SIGNUP_APPLE = "$BASE_URL/signup/APPLE"
-        const val SIGNUP_NAVER = "$BASE_URL/signup/NAVER"
         const val REISSUE = "$BASE_URL/reissue"
         const val WITHDRAW_KAKAO = "$BASE_URL/KAKAO"
         const val WITHDRAW_APPLE = "$BASE_URL/APPLE"
