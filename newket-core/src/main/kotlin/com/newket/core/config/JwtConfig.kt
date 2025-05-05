931259f2bc579a61a1ce3839245d4dd28731046f
sominyun <somin455@naver.com>
2025-05-06 01:16:12 +0900

    feat:#11 cors origin 추가

--- newket-core/src/main/kotlin/com/newket/core/config/JwtConfig.kt
+++ newket-core/src/main/kotlin/com/newket/core/config/JwtConfig.kt
@@ -1,6 +1,5 @@
 package com.newket.core.config
 
-import com.newket.core.auth.AppleProperties
 import com.newket.core.auth.JwtProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
