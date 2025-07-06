9affc80c1d435c7e85f18af0a8b38bd99587dfc7
sominyun <somin455@naver.com>
2025-07-06 21:31:39 +0900

    fix:#18 push 알림

--- newket-scheduler/src/main/kotlin/com/newket/scheduler/NotificationScheduler.kt
+++ newket-scheduler/src/main/kotlin/com/newket/scheduler/NotificationScheduler.kt
@@ -10,32 +10,32 @@ class NotificationScheduler(
     private val notificationManager: NotificationManager,
     private val ticketCacheManager: TicketCacheManager
 ) {
-//    // 추가 예매
-//    @Scheduled(cron = "0 00 10 * * *") //매일 오전 10시
-//    fun addTicketSale() {
-//        ticketCacheManager.updateTicketSaleAndSendArtistNotification()
-//    }
-//
-//    // 아티스트 추가
-//    @Scheduled(cron = "0 00 14 * * *") //매일 오후 2시
-//    fun addTicketArtist() {
-//        ticketCacheManager.updateTicketArtistAndSendArtistNotification()
-//    }
-//
-//    // 새로운 티켓
-//    @Scheduled(cron = "0 00 14 * * *") //매일 오후 2시
-//    fun newTicketOpen() {
-//        ticketCacheManager.createTicketCacheAndSendArtistNotification()
-//    }
-//
-//    @Scheduled(cron = "0 00 * * * *") //매시간
-//    fun ticketOpenBefore1hour() {
-//        notificationManager.sendTicketOpenBefore1hour()
-//        notificationManager.sendTicketOpenBeforeDay()
-//    }
-//
-//    @Scheduled(cron = "0 0 0 * * *") // 매일 자정 12시
-//    fun deleteAllOldTicketCache() {
-//        ticketCacheManager.deleteAllAfterSaleTicketCache()
-//    }
+    // 추가 예매
+    @Scheduled(cron = "0 00 10 * * *") //매일 오전 10시
+    fun addTicketSale() {
+        ticketCacheManager.updateTicketSaleAndSendArtistNotification()
+    }
+
+    // 아티스트 추가
+    @Scheduled(cron = "0 00 14 * * *") //매일 오후 2시
+    fun addTicketArtist() {
+        ticketCacheManager.updateTicketArtistAndSendArtistNotification()
+    }
+
+    // 새로운 티켓
+    @Scheduled(cron = "0 00 14 * * *") //매일 오후 2시
+    fun newTicketOpen() {
+        ticketCacheManager.createTicketCacheAndSendArtistNotification()
+    }
+
+    @Scheduled(cron = "0 00 * * * *") //매시간
+    fun ticketOpenBefore1hour() {
+        notificationManager.sendTicketOpenBefore1hour()
+        notificationManager.sendTicketOpenBeforeDay()
+    }
+
+    @Scheduled(cron = "0 0 0 * * *") // 매일 자정 12시
+    fun deleteAllOldTicketCache() {
+        ticketCacheManager.deleteAllAfterSaleTicketCache()
+    }
 }
